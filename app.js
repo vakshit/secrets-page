7a4f30903d04a6b0920e7723c9cea7fcfacadbe9
vakshit <akshitv18@gmail.com>
2021-08-27 11:44:14 +0530

    Lvl4 Added Hashing

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -22,9 +23,6 @@ const userSchema = new mongoose.Schema( {
   password: String
 });
 
-const secret = process.env.SECRET // encryption key
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']}) // for encrypting password field
-
 const User = mongoose.model('User', userSchema);
 
 
@@ -39,7 +37,7 @@ app.route('/login')
 
 .post(function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if (err){
@@ -57,8 +55,6 @@ app.route('/login')
 });
 
 
-
-
 app.route('/register')
 .get(function(req, res) {
   res.render('register');
@@ -67,7 +63,7 @@ app.route('/register')
 .post(function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if (err){
