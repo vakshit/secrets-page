5e87d8366654bd3cc544e9170af43e8d0484dfaf
vakshit <akshitv18@gmail.com>
2021-08-28 00:46:43 +0530

    Lvl5 Added bycrypt

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 
@@ -37,18 +38,16 @@ app.route('/login')
 
 .post(function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
-
   User.findOne({email: username}, function(err, foundUser){
     if (err){
       res.send(err);
     } else {
       if (foundUser){
-        if (foundUser.password === password) {
-          res.render('secrets');
-        }else {
-
-        }
+        bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render('secrets');
+          }
+        });
       }
     }
   })
@@ -61,16 +60,19 @@ app.route('/register')
 })
 
 .post(function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if (err){
-      res.send(err);
-    } else {
-      res.render('secrets');
-    }
+  
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err){
+        res.send(err);
+      } else {
+        res.render('secrets');
+      }
+    });
   });
 });
 
